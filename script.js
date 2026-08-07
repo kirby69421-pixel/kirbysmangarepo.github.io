5d39ac3a1d059a81c993f98f901abfbcb38e670b
kirby69421-pixel <kirby69421@gmail.com>
2026-08-06 21:53:32 -0400

    Add initial data for 'Four Knights of the Apocalypse'

---
+++ script.js
@@ -0,0 +1,9 @@
+{
+    "Four Knights of the Apocalypse": {
+        "cover":"manga/Four Knights of the Apocalypse/cover.webp",
+        "chapters":{
+            "001":18,
+            "002":19
+        }
+    }
+}
