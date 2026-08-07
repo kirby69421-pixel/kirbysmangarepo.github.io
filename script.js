1fb23eda1fb594df78ca526e44c0c70fa31b715b
kirby69421-pixel <kirby69421@gmail.com>
2026-08-06 22:00:58 -0400

    Implement manga library fetching and display
    Fetch library data from JSON and display manga titles.

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-{
-    "Four Knights of the Apocalypse": {
-        "cover":"manga/Four Knights of the Apocalypse/cover.webp",
-        "chapters":{
-            "001":18,
-            "002":19
-        }
-    }
-}
+let library;
+
+
+fetch("library.json")
+.then(response => response.json())
+.then(data => {
+
+    library = data;
+
+    let container = document.getElementById("library");
+
+
+    Object.keys(library).forEach(manga => {
+
+        let box = document.createElement("div");
+
+        box.className = "manga";
+
+
+        box.innerHTML = `
+            <h2>${manga}</h2>
+
+            <a href="reader.html?manga=${encodeURIComponent(manga)}&chapter=1">
+                Start Reading
+            </a>
+        `;
+
+
+        container.appendChild(box);
+
+    });
+
+});
